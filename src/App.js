eb577112d92577ef644ce43c2df44aae587d38f0
Martin Trembac <martintrembac052@gmail.com>
2023-07-11 19:42:23 +0100

    changed id to image line 84

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
 
-    fetch("https://mybackend-3m9h.onrender.com/imageurl", {
+    fetch('https://mybackend-3m9h.onrender.com/imageurl', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
