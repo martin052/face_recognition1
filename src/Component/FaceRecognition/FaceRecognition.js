f0a7620d260059799927b006093a156ad8041135
Martin Trembac <martintrembac052@gmail.com>
2023-07-11 19:39:58 +0100

    changed id to image line 84

--- src/Component/FaceRecognition/FaceRecognition.js
+++ src/Component/FaceRecognition/FaceRecognition.js
@@ -1,29 +1,25 @@
-import React from "react";
-import './FaceRecognition.css'
+import React from 'react';
+import './FaceRecognition.css';
 
-// const FaceRecognition = ({ imageUrl, box }) => {
-//     return (
-//         <div className="center ma">
-//             <div className="absolute mt2 ">
-//                 <img id='inputImage' alt="" src={imageUrl} width='500px' height='auto' />
-//                 <div className="bounding-box" style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
-//             </div>
-//         </div>
-//     )
-// }
-const FaceRecognition = ({ imageUrl, boxes }) => {
+export default function FaceRecognition({ imageURL, boxes }) {
+    const styles = {
+        top: boxes.topRow,
+        right: boxes.rightCol,
+        bottom: boxes.bottomRow,
+        left: boxes.leftCol
+    }
+
+
     return (
-        <div className="center ma">
-            <div className="absolute mt2 ">
-                <img id='inputImage' alt="" src={imageUrl} width='500px' height='auto' />
-                {
-                    boxes.map((box, i) => {
-                        const { topRow, rightCol, bottomRow, leftCol } = box;
-                        return (<div key={i} className="bounding-box" style={{ top: topRow, right: rightCol, bottom: bottomRow, left: leftCol }}></div>);
-                    })}
+        <div className='center ma'>
+            <div className='absolute mt2'>
+                <img id="inputimage" alt="" src={imageURL} className="mt3" width="500px" height="auto" />
+                {boxeses.map((boxes, i) => {
+                    return (
+                        <div key={i} className='bounding-boxes' style={styles}></div>
+                    )
+                })}
             </div>
         </div>
     )
 }
-
-export default FaceRecognition;
